0c2158694c4a449e8058d077c1bb6d8a25bd6d16
rohit <rohitg9421@gmail.com>
2026-02-17 00:33:05 +0530

    86 file added

--- push_image.bat
+++ push_image.bat
@@ -1,8 +1,23 @@
 @echo off
+
+REM Ask for version name (for branch)
+set /p VERSION=Enter Version Name (for branch): 
+
+REM Ask for image tag (for commit)
+set /p TAG=Enter Image Tag (for commit message): 
+
 C:\Users\rohit\Downloads\Efficient-Anomaly-Detector
 
-git add .
-git commit -m "Auto push image update"
-git push origin main
+REM Create new branch
+"C:\Program Files\Git\cmd\git.exe" checkout -b %VERSION%
+
+REM Add files
+"C:\Program Files\Git\cmd\git.exe" add .
+
+REM Commit with image tag
+"C:\Program Files\Git\cmd\git.exe" commit -m "%TAG% file added"
+
+REM Push branch
+"C:\Program Files\Git\cmd\git.exe" push -u origin %VERSION%
 
 pause
